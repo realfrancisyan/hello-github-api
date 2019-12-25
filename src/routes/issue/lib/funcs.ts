990ad58acd8258ad6939dfb782aa214c5cd221c7
Jiajun Yan <yanjiajun@nome.com>
2019-12-26 00:10:38 +0800

    修复无法连接代理问题

--- src/routes/issue/lib/funcs.ts
+++ src/routes/issue/lib/funcs.ts
@@ -211,7 +211,7 @@ export const getOrderedContent = (content: []) => {
 // 获取所有期数
 export const _getIssueCounts = async () => {
   const proxyUrl = `${TARGET}/tree/master/content`;
-  const data = await fetchWithProxy(proxyUrl);
+  const data = await fetchWithoutProxy(proxyUrl);
   const $ = cheerio.load(await data.text());
 
   const issues = $('tr')
@@ -241,7 +241,7 @@ export const _getIssueCountsFromDB = async () => {
 export const _getIssue = async (id: string) => {
   id = id.padStart(2, '0'); // 填充 0，如 1 => 01
   const proxyUrl = `${TARGET}/blob/master/content/${id}/HelloGitHub${id}.md`;
-  const data = await fetchWithProxy(proxyUrl);
+  const data = await fetchWithoutProxy(proxyUrl);
   const $ = cheerio.load(await data.text());
 
   const categories = $('article h3')
