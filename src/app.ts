0bb43f608662168f05ac478a58ee82719fda7312
Jiajun Yan <yanjiajun@nome.com>
2019-12-26 10:59:47 +0800

    添加任务，定时获取最新一期

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as Koa from 'koa';
 import * as json from 'koa-json';
 import * as bodyParser from 'koa-body';
 import applyApiMiddleware from './routes';
+import { scheduleGetLatestIssue } from './tasks';
 
 const app = new Koa();
 
@@ -19,4 +20,5 @@ applyApiMiddleware(app);
 const port = 4000;
 app.listen(port, () => {
   console.log(`Server has started at http://localhost:${port}`);
+  scheduleGetLatestIssue();
 });
---
+++ src/tasks/index.ts
@@ -0,0 +1,33 @@
+import * as schedule from 'node-schedule';
+import {
+  _getIssueCountsFromDB,
+  _getIssue,
+  savePosts
+} from '../routes/issue/lib/funcs';
+
+let job: any = null;
+
+// 获取最新一期
+const getLatestIssue = async () => {
+  const latestIssueInDB = await _getIssueCountsFromDB(); // 获取数据库最新一期
+
+  const latestIssue = latestIssueInDB + 1 + '';
+  console.log(latestIssue);
+  // 保存内容
+  const categories = await _getIssue(latestIssue);
+  await savePosts(categories, latestIssue);
+};
+
+// 定期获取最新一期
+export const scheduleGetLatestIssue = async () => {
+  const rule = { hour: 20, minute: 10 }; // repeat every 28th at 20:10
+
+  console.log('init job - get latest issue');
+
+  job = schedule.scheduleJob(rule, async () => {
+    console.log('Beginning get latest issue schedule... ' + new Date());
+    getLatestIssue();
+  });
+
+  console.log('end init job - get latest issue');
+};
