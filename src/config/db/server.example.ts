bd0e3af4539139f2905677f58eca550b3edde9bc
Jiajun Yan <yanjiajun@nome.com>
2020-01-09 14:53:14 +0800

    添加数据库示例文件

---
+++ src/config/db/server.example.ts
@@ -0,0 +1,10 @@
+// 本项目使用 mongodb
+// db address
+const ADDRESS = '0.0.0.0'; // 数据库 地址
+const PORT = 27017; // 数据库端口
+
+// db auth
+const USER = 'user'; // 用户名
+const PASSWORD = 'password'; // 密码
+
+export { ADDRESS, PORT, USER, PASSWORD };
