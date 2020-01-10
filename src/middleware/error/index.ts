2726393aaab69916ebfdb82dcc955a9a78d12661
Jiajun Yan <yanjiajun@nome.com>
2020-01-10 16:41:34 +0800

    添加 return

--- src/middleware/error/index.ts
+++ src/middleware/error/index.ts
@@ -2,7 +2,7 @@ import * as Koa from 'koa';
 
 export const handleError = async (ctx: Koa.Context, next: Function) => {
   try {
-    await next();
+    return await next();
   } catch (err) {
     ctx.status = err.status || 500;
     ctx.body = err.message;
